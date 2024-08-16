43265a63f11442914ec8727cce6c871714fa7b3e
John Oye <olugbengajohnoye@gmail.com>
2024-08-16 15:55:16 +0100

    Made a lot of changes to make it handle more requests

--- src/hermes/Main.java
+++ src/hermes/Main.java
@@ -1,6 +1,7 @@
 package hermes;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,127 +10,148 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-	
-	public static Map<String, Object> database;
-	public static StringBuilder logs;
-	public static String rootDir = System.getProperty("user.home") + "/Project-Hermes";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			// Initialize variables
-			database = new HashMap<String, Object>();
-			logs = new StringBuilder("");
-			// Initiate server
-			ServerSocket ss = new ServerSocket(2907);
-			
-			// Welcome message
-			System.out.println("-- Hermes Initiated --"); 
-			
-			while(true) {
-				// Accept incoming requests
-				Socket s = ss.accept();
-			
-				// Read the input stream for data from the request
-				BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			
-				String line = br.readLine();
-				
-				
-				// Print the request
-				System.out.println(line);
-				
-				if(!line.toLowerCase().startsWith("exit")) logs.append(line + "\n");
-				
-				
-				// Send a response to the client
-				PrintWriter out = new PrintWriter(new OutputStreamWriter(s.getOutputStream()), true);
-				out.println(handleRequest(line));
-				
-			}
-			
-		} catch (IOException e) {
-			System.err.println("Could not start server");
-		}
-	}
-	
-	public static String get(String key) {
-		if(database.containsKey(key)) {
-			return (String) database.get(key);
-		}
-		
-		return "Could not find data with key: "+key;
-	}
-	
-	public static void set(String key, String[] values) {
-		StringBuilder sb = new StringBuilder("");
-		
-		for(int i = 2; i < values.length; i++) {
-			sb.append(values[i] + " ");
-		}
-		database.put(key, sb.toString().stripTrailing());
-	}
-	
-	public static void saveLog() {
-		try {
-			File logDir = new File(rootDir + File.separator + "logs");
-			File logFile = new File(rootDir + File.separator + "logs" + File.separator + "req_log.herm");
-			logDir.mkdirs();
-			
-			if(!logFile.exists()) logFile.createNewFile();
-			
-			FileWriter fw = new FileWriter(logFile);
-			fw.append(logs.toString());
-			
-			fw.flush();
-			fw.close();
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static String getAll() {
-		StringBuilder sb = new StringBuilder("");
-		sb.append("{");
-		
-		for(String key : database.keySet()) {
-			sb.append(key + ": " + '"' + database.get(key) + '"' + ", ");
-			
-		}
-		
-		sb.delete(sb.length()-2, sb.length());
-		
-		sb.append("}");
-		
-		
-		return sb.toString();
-	}
-	
-	public static String handleRequest(String request) {
-		String[] formatted = request.split(" ");
-		
-		// Using a switch-case for better performance
-		switch(formatted[0].toLowerCase()) {
-			case "get":
-				return get(formatted[1]);
-			case "set":
-				set(formatted[1], formatted);
-				return "Set successfully";
-			case "exit":
-				saveLog();
-				System.exit(0);
-				break;
-			case "all":
-				 return getAll();
-		}
-		
-		return "Seems there was a problem with your request";
-	}
+    private static final Map<String, Object> database = new ConcurrentHashMap<>();
+    private static final StringBuilder logs = new StringBuilder();
+    private static final String rootDir = System.getProperty("user.home") + "/Project-Hermes";
+    private static final String logFileAddr = rootDir + File.separator + "logs" + File.separator + "req_log.herm";
+    private static final int THREAD_POOL_SIZE = 1000;
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
+    public static void main(String[] args) {
+        try (ServerSocket ss = new ServerSocket(2907)) {
+            System.out.println("-- Hermes Initiated --");
+
+            while (true) {
+                Socket s = ss.accept();
+                threadPool.execute(() -> handleClient(s));
+            }
+        } catch (IOException e) {
+            System.err.println("Could not start server");
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+            saveLog();
+        }
+    }
+
+    private static void handleClient(Socket socket) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    System.out.println(line);
+                    logs.append(line).append("\n");
+                    out.println(handleRequest(line));
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String get(String key) {
+        return database.containsKey(key) ? (String) database.get(key) : "Could not find data with key: " + key;
+    }
+
+    private static void set(String key, String[] values) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i < values.length; i++) {
+            sb.append(values[i]).append(" ");
+        }
+        database.put(key, sb.toString().stripTrailing());
+    }
+
+    private static void saveLog() {
+        File logDir = new File(rootDir + File.separator + "logs");
+        File logFile = new File(logFileAddr);
+        logDir.mkdirs();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true))) {
+            writer.append(logs.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void bootFromLog() {
+        File logFile = new File(logFileAddr);
+        if (logFile.exists()) {
+            try (Scanner sc = new Scanner(logFile).useDelimiter("\n")) {
+                while (sc.hasNext()) {
+                    handleRequest(sc.nextLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String getAll() {
+        return database.toString();
+    }
+
+    private static void add(String key, String value) {
+        try {
+            int addition = Integer.parseInt(value);
+            int val = Integer.parseInt(get(key));
+            database.put(key, String.valueOf(val + addition));
+        } catch (NumberFormatException e) {
+            database.put(key, get(key) + value);
+        }
+    }
+
+    private static String concat(String key, String[] values) {
+        StringBuilder sb = new StringBuilder(get(key));
+        for (int i = 2; i < values.length; i++) {
+            sb.append(values[i]).append(" ");
+        }
+        database.put(key, sb.toString().stripTrailing());
+        return "Successfully concatenated to string: " + key;
+    }
+
+    private static String handleRequest(String request) {
+        try {
+            String[] formatted = request.split(" ");
+            switch (formatted[0].toLowerCase()) {
+                case "get":
+                    return get(formatted[1]);
+                case "set":
+                    set(formatted[1], formatted);
+                    return "Set successfully";
+                case "exit":
+                    saveLog();
+                    System.exit(0);
+                    break;
+                case "all":
+                    return getAll();
+                case "add":
+                    add(formatted[1], formatted[2]);
+                    return "Added successfully";
+                case "concat":
+                    return concat(formatted[1], formatted);
+                default:
+                    return "Invalid command";
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Wrong number of arguments";
+        }
+        return "Unhandled request";
+    }
 }
